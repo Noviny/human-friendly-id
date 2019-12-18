a5fc8dbb3efd86aa08a648678fb7e339acfebb61
Ben Conolly <ben@thinkmill.com.au>
2019-12-18 14:30:15 +1100

    small tweaks

---
+++ README.md
@@ -0,0 +1,5 @@
+# Human Friendly Ids
+
+This is very WIP and currently just for fun. Probably do not try and use this for anything.
+
+If you want to contribute friendly words, that would be grand :)
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const adjectives = ["friendly"];
 const nouns = ["humans"];
 const verbs = ["welcome"];
 
-const getFriendlyId = () =>
-  `${getRandomItem(adjectives)}-${getRandomItem(nouns)}-${getRandomItem(
-    verbs
-  )}`;
+// prettier-ignore
+const getFriendlyId = () => `${getRandomItem(adjectives)}-${getRandomItem(nouns)}-${getRandomItem(verbs)}`;
+
+module.exports = getFriendlyId;
