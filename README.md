7896f130b28456c5086ad37a6d78ce414aadd425
Ben Conolly <ben@thinkmill.com.au>
2019-12-18 14:37:14 +1100

    I have all the friends

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 This is very WIP and currently just for fun. Probably do not try and use this for anything.
 
+This project is inspired by [human-id](https://www.npmjs.com/package/human-id) which you should use instead.
+
+I wanted to make my own version that is the same thing but with only words I love and a little bit more jokey
+
 If you want to contribute friendly words, that would be grand :)
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const getRandomItem = arr => arr[Math.floor(Math.random() * arr.length) + 1];
 
-const adjectives = ["friendly"];
-const nouns = ["humans"];
-const verbs = ["welcome"];
+const adjectives = ["friendly", "kind", "cute", "adorbs"];
+const nouns = ["humans", "butterflies", "packages", "friends"];
+const verbs = [
+  "welcome",
+  "do-crime",
+  "are-beautiful",
+  "love",
+  "have-all-the-friends"
+];
 
 // prettier-ignore
 const getFriendlyId = () => `${getRandomItem(adjectives)}-${getRandomItem(nouns)}-${getRandomItem(verbs)}`;
